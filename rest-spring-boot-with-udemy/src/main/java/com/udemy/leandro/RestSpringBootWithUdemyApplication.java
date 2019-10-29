72a7751678b5bb2a651cdb76476f97800c4c9024
Leandro Reis <developer.reis@gmail.com>
2019-10-29 11:40:43 -0300

    Greeting

---
+++ rest-spring-boot-with-udemy/src/main/java/com/udemy/leandro/RestSpringBootWithUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.leandro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringBootWithUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringBootWithUdemyApplication.class, args);
+	}
+
+}
