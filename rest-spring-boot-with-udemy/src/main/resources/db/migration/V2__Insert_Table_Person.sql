045e79631aa26ab981c775d6261649873b9d52b6
Leandro Reis <developer.reis@gmail.com>
2019-11-01 17:33:36 -0300

    flyway migration

---
+++ rest-spring-boot-with-udemy/src/main/resources/db/migration/V2__Insert_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `adress`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'uberlandia', 'ricardo', 'male', 'Reis'),
+	(3, 'rio de janeiro', 'josue', 'male', 'Reis'),
+	(5, 'Sao Paulo', 'gabrielass', 'female', 'Reis'),
+	(6, 'Curitiba', 'Leandro', 'male', 'Reis'),
+	(7, 'Curitiba', 'Joyce', 'female', 'calandro');
